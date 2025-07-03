487fed5b766bc3aa1b61c2bfd5e0552731667144
Иван <127229725+ivaneremeya@users.noreply.github.com>
2025-07-03 15:57:48 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   
-  base: '/testFull/',
+  base: '/testsFull/',
   plugins: [
     vue(),
     vueDevTools(),
